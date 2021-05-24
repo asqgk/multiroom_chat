75d731de0071956253afa2ef812eecb2dc8a20e7
Francisco dos Passos <chico.81@hotmail.com>
2021-05-23 21:30:25 -0300

    updated participants screen

--- app.js
+++ app.js
@@ -6,11 +6,40 @@ var server = app.listen(80, function(){
 
 var io = require('socket.io').listen(server);
 
+app.set('io', io);
+
 // criar a conexão por websocket
 io.on("connection", function(socket){
     console.log("Usuário conectou");
 
     socket.on("disconnect", function(){
         console.log("Usuário desconectou");
-    })
+    });
+
+    socket.on('msgParaServidor', function(data){
+        /* dialogo */
+        socket.emit(
+            'msgParaCliente',
+            { apelido: data.apelido, mensagem: data.mensagem }
+        );
+
+        socket.broadcast.emit(
+            'msgParaCliente',
+            { apelido: data.apelido, mensagem: data.mensagem }
+        );
+
+        /* participantes */
+        if(parseInt(data.apelido_atualizado_nos_clientes) == 0){
+            socket.emit(
+                'participantesParaCliente',
+                { apelido: data.apelido }
+            );
+
+            socket.broadcast.emit(
+                'participantesParaCliente',
+                { apelido: data.apelido }
+            );
+        }
+    });
+    
 });
